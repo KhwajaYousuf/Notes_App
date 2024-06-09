9a5d302619da31b90f349ad3af663078419ab796
Yousuf <kyousufmo17@gmail.com>
2024-06-09 16:23:48 -0400

    Added click event listener to create button in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const notesContainer = document.querySelector(".notes-container")
 const createBtn = document.querySelector(".btn")
 let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", ()=>{
+    
+})
